3b8da972a74bfca3c3437b925da25936b95d0c47
Latet Jasper <71765441+jasperLatet@users.noreply.github.com>
2024-05-19 20:48:24 +0200

    Test om geschiedenis te verwijderen

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const pg = require('pg');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors')
+require('dotenv').config();
+
+
+const port=3000;
+
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    host: 'db',
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    connectionTimeoutMillis: 5000
+})
+
+console.log("Connecting...:")
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/authenticate/:username/:password', async (request, response) => {
+    const username = request.params.username;
+    const password = request.params.password;
+
+    const query = `SELECT * FROM users WHERE user_name=$1 AND password=crypt($2, password)`;
+    console.log(query);
+    pool.query(query, [username, password], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)});
+      
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
